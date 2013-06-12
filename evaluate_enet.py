786e39dfb07951994a4cb39ad36fd5030c5961d3
Joshua Lynch <joshua.kevin.lynch@gmail.com>
2013-06-11 21:47:33 -0500

    classification_report does not seem to work with regression

--- evaluate_enet.py
+++ evaluate_enet.py
@@ -32,20 +32,29 @@ def evaluate_enet():
     scaler = sklearn.preprocessing.StandardScaler()
     # the scaler returns a copy by default
     X = scaler.fit_transform(shared_data.otu_frequency)
-    # X and class_number_for_row are ready
-
-    elastic_net(X, design_data.class_number_for_row[:,0], shared_data.otu_column_names)
+    y = design_data.class_number_for_row[:,0]
+    y_labels = [design_data.class_number_to_name[n] for n in sorted(design_data.class_number_to_name.keys())]
+    print(y_labels)
+    elastic_net(X, y, y_labels, shared_data.otu_column_names)
 
 
 """
 This function performs feature selection using elastic net.  The hyperparameters
 l1_ratio and alpha are determined by cross validation.
 """
-def elastic_net(X, y, otu_column_names):
+def elastic_net(X, y, y_labels, otu_column_names):
+    sss = sklearn.cross_validation.StratifiedShuffleSplit(
+        y, test_size=0.5
+    )
+    train_index, test_index = next(iter(sss))
+    X_train = X[train_index, :]
+    X_test = X[test_index, :]
+    y_train = y[train_index]
+    y_test = y[test_index]
    
     ##############################################################################
-    n_train = int( 4.0/5.0 * y.shape[0] )
-    print('n_train: {}'.format(n_train))
+    #n_train = int( 4.0/5.0 * y.shape[0] )
+    #print('n_train: {}'.format(n_train))
     best_model = None
     best_model_score = 0.0
     # I could not get ElasticNetCV to choose the best l1_ratio from a list
@@ -53,15 +62,16 @@ def elastic_net(X, y, otu_column_names):
     for l1_ratio in [.1, .2, .3, .4]:
         model = sklearn.linear_model.ElasticNetCV(
             l1_ratio=l1_ratio,
-            cv=sklearn.cross_validation.StratifiedKFold(y[n_train:], 3),
+            #cv=sklearn.cross_validation.StratifiedKFold(y[n_train:], 3),
+            cv=5,
             verbose=False
-        ).fit(X[n_train:, :], y[n_train:])
+        ).fit(X_train, y_train)
         # I think using model.score was the wrong way to go
-        model_score_xx = model.score(X[:n_train,:], y[:n_train])
+        model_score_xx = model.score(X_test, y_test)
         print('built-in model.score: {}'.format(model_score_xx))
-        test_prediction = model.predict(X[:n_train])
+        test_prediction = model.predict(X_test)
         test_classification = test_prediction.round()
-        test_classification_correct = (y[:n_train] == test_classification)
+        test_classification_correct = (y_test == test_classification)
         model_score = test_classification_correct.sum() / float(len(test_classification_correct))
         if model_score > best_model_score:
             best_model = model
@@ -75,7 +85,11 @@ def elastic_net(X, y, otu_column_names):
         enet_top_feature_list = get_enet_top_features(model)
         for n, (feature_ndx, rank) in enumerate(enet_top_feature_list[:10]):
             print('{:2d} {} {:4.2f}'.format(n, otu_column_names[feature_ndx], rank))
-    
+
+    y_true, y_pred = y_test, best_model.predict(X_test)
+    print(sklearn.metrics.classification_report(y_true, y_pred))
+    print('')
+
     print('best l1_ratio: {}'.format(best_model.l1_ratio_))
     print('best alpha:    {}'.format(best_model.alpha_))
     print('best model score: {}'.format(best_model_score))
